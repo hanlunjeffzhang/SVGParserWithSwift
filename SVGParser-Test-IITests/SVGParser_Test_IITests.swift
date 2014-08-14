1b04fd0088656012ba5fade169c9590397dc0d0e
hanlunjeffzhang <jeff@danningdeMacBook-Pro.local>
2014-08-14 15:37:16 +0800

    Creation

---
+++ SVGParser-Test-IITests/SVGParser_Test_IITests.swift
@@ -0,0 +1,36 @@
+//
+//  SVGParser_Test_IITests.swift
+//  SVGParser-Test-IITests
+//
+//  Created by Jeff on 2014-08-08.
+//  Copyright (c) 2014 Jeff. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SVGParser_Test_IITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
